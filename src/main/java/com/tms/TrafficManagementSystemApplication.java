36f1fa5aa4145783e58bf586f8a70f7b96c9506e
Balaji239 <baji2391999@gmail.com>
2024-06-24 20:18:39 +0530

    project setup

---
+++ src/main/java/com/tms/TrafficManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficManagementSystemApplication.class, args);
+	}
+
+}
